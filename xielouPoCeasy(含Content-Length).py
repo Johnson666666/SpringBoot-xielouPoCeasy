08b915a216626e6d355f709767fbcc0d6fd4180b
Johnson666666 <72605468+Johnson666666@users.noreply.github.com>
2021-09-06 13:56:18 +0800

    Update xielouPoCeasy(含Content-Length).py

--- xielouPoCeasy(含Content-Length).py
+++ xielouPoCeasy(含Content-Length).py
@@ -4,12 +4,13 @@ import time
 url = 'https://msg.going-link.com/'     #这里url末尾要有'/'
 with open("SpringBoot信息泄露目录字典.txt", 'r') as web:
     webs = web.readlines()
+w = open('easyresult.txt', 'w+')
 for web in webs:
     web = web.strip()
     u = url + web
     response = requests.get(u)
+    
     #print("url为:"+u)
     print("url为:" + u + ' ' + "状态为:%d"%response.status_code + ' ' + "content-length为:" + str(len(response.content)))
     time.sleep(5)       #想睡多久看自己~
-w = open('easyresult.txt', 'w+')
-w.write("url为:" + u + ' ' + "状态为:%d"%response.status_code + ' ' + "content-length为:" + str(len(response.content)) + '\n')
+    w.write("url为:" + u + ' ' + "状态为:%d"%response.status_code + ' ' + "content-length为:" + str(len(response.content)) + '\n')
